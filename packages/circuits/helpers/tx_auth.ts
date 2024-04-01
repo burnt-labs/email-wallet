dee01f5b98576b5d687b9d0388f525291d19de29
zkFriendly <zkfriendly@skiff.com>
2024-04-01 11:57:53 +0200

    feat: add tx body and salt start index

--- packages/circuits/helpers/tx_auth.ts
+++ packages/circuits/helpers/tx_auth.ts
@@ -7,17 +7,18 @@ export type TxAuthCircuitInput = {
   in_padded: string[];
   pubkey: string[];
   signature: string[];
+  tx_body_idx: string;
+  tx_salt_idx: string;
   in_len_padded_bytes: string;
   precomputed_sha?: string[];
   in_body_padded?: string[];
   in_body_len_padded_bytes?: string;
   body_hash_idx?: string;
-}
+};
 
 export async function genTxAuthInputs(emailFilePath: string): Promise<TxAuthCircuitInput> {
-  
   const max_message_length = 1024;
-  const max_body_length = 1024;
+  const max_body_length = 2048;
 
   const emailRaw = await promisify(fs.readFile)(emailFilePath, "utf8");
   const dkimResult = await verifyDKIMSignature(Buffer.from(emailRaw));
@@ -28,8 +29,23 @@ export async function genTxAuthInputs(emailFilePath: string): Promise<TxAuthCirc
     bodyHash: dkimResult.bodyHash,
     message: dkimResult.message,
     maxMessageLength: max_message_length,
-    maxBodyLength: max_body_length
+    maxBodyLength: max_body_length,
   });
 
-  return emailCircuitInputs;
+  const data = emailCircuitInputs.in_padded!.map((x) => Number(x));
+
+  // get index of all `#` in data
+  const idx = data.reduce((a: number[], e, i) => {
+    if (e === 35) a.push(i);
+    return a;
+  }, []);
+
+  const tx_body_idx = idx[0] + 1;
+  const tx_salt_idx = idx[1] + 1;
+
+  return {
+    ...emailCircuitInputs,
+    tx_body_idx: tx_body_idx.toString(),
+    tx_salt_idx: tx_salt_idx.toString(),
+  };
 }
