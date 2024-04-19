45e849f0749d8a08dfd58f3edbcc4313d8bf0025
zkFriendly <zkfriendly@proton.me>
2024-04-19 15:16:07 +0200

    test: serialize/deserialize vkey

--- packages/bindings/src/ark_verifier/mod.rs
+++ packages/bindings/src/ark_verifier/mod.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use ark_bn254::Bn254;
     use ark_circom::read_zkey;
     use ark_circom::CircomBuilder;
@@ -7,6 +9,9 @@ mod tests {
     use ark_circom::CircomReduction;
     use ark_crypto_primitives::snark::SNARK;
     use ark_groth16::Groth16;
+    use ark_groth16::VerifyingKey;
+    use ark_serialize::CanonicalDeserialize;
+    use ark_serialize::CanonicalSerialize;
     use ark_std::rand::thread_rng;
     type GrothBn = Groth16<Bn254, CircomReduction>;
 
@@ -22,13 +27,18 @@ mod tests {
         builder.push_input("in", 1);
         builder.push_input("in", 2);
 
+        let vk =
+            VerifyingKey::<Bn254>::deserialize_compressed(fs::File::open("vk").unwrap()).unwrap();
         let circuit = builder.build().unwrap();
         let mut rng = thread_rng();
         let mut reader = std::fs::File::open("../circuits/build/dummy_circuit.zkey").unwrap();
         let pk = read_zkey(&mut reader).unwrap().0;
         let public_inputs = circuit.get_public_inputs().unwrap();
         let proof = GrothBn::create_random_proof_with_reduction(circuit, &pk, &mut rng).unwrap();
-        let verified = GrothBn::verify(&pk.vk, &public_inputs, &proof).unwrap();
+        let verified = GrothBn::verify(&vk, &public_inputs, &proof).unwrap();
+
+        let writer: fs::File = fs::File::create("vk").unwrap();
+        pk.vk.serialize_compressed(writer).unwrap();
 
         assert!(verified);
     }
