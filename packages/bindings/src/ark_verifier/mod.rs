3dfbdfc2d50361d033fecc970fbaba9961a653fa
zkFriendly <zkfriendly@proton.me>
2024-04-19 14:57:24 +0200

    test: load proving key from zkey file and generate and validate proofs

--- packages/bindings/src/ark_verifier/mod.rs
+++ packages/bindings/src/ark_verifier/mod.rs
@@ -1,4 +1,35 @@
-// Module: ark_verifier
-pub fn verify() {
-    println!("Ark verifier");
+#[cfg(test)]
+mod tests {
+    use ark_bn254::Bn254;
+    use ark_circom::read_zkey;
+    use ark_circom::CircomBuilder;
+    use ark_circom::CircomConfig;
+    use ark_circom::CircomReduction;
+    use ark_crypto_primitives::snark::SNARK;
+    use ark_groth16::Groth16;
+    use ark_std::rand::thread_rng;
+    type GrothBn = Groth16<Bn254, CircomReduction>;
+
+    #[test]
+    fn test_dummy_circuit_prover() {
+        let cfg: CircomConfig<Bn254> = CircomConfig::<Bn254>::new(
+            "../circuits/build/dummy_circuit/dummy_circuit_js/dummy_circuit.wasm",
+            "../circuits/build/dummy_circuit/dummy_circuit.r1cs",
+        )
+        .unwrap();
+
+        let mut builder = CircomBuilder::new(cfg);
+        builder.push_input("in", 1);
+        builder.push_input("in", 2);
+
+        let circuit = builder.build().unwrap();
+        let mut rng = thread_rng();
+        let mut reader = std::fs::File::open("../circuits/build/dummy_circuit.zkey").unwrap();
+        let pk = read_zkey(&mut reader).unwrap().0;
+        let public_inputs = circuit.get_public_inputs().unwrap();
+        let proof = GrothBn::create_random_proof_with_reduction(circuit, &pk, &mut rng).unwrap();
+        let verified = GrothBn::verify(&pk.vk, &public_inputs, &proof).unwrap();
+
+        assert!(verified);
+    }
 }
--- packages/bindings/src/main.rs
+++ packages/bindings/src/main.rs
@@ -1,7 +1,5 @@
 mod ark_verifier;
 
-use ark_verifier::verify;
-
 fn main() {
-    verify();
+    println!("Hello, world!");
 }
