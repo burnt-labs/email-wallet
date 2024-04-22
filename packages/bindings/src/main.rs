2f940ff95b2ab72d6e0340a851352b3937568b1f
zkFriendly <zkfriendly@proton.me>
2024-04-22 13:32:08 +0200

    feat: mini cli to generate vkey from zkey

--- packages/bindings/src/main.rs
+++ packages/bindings/src/main.rs
@@ -1,5 +1,20 @@
 mod ark_verifier;
+use clap::Parser;
+
+/// CLI to generate ark-circom compatible verification keys from zkey files.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// snarkjs zkey file path
+    #[arg(short, long)]
+    zkey: String,
+
+    /// output file path for the verification key
+    #[arg(short, long, default_value = "./vk.rvkey")]
+    rvkey: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    ark_verifier::generate_vkey_file_from_zkey(&args.zkey, &args.rvkey);
 }
