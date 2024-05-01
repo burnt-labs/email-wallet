96ba9d791e83a2babc4f82df11a3d6a14636ab5a
zkFriendly <zkfriendly@proton.me>
2024-05-01 23:17:51 +0200

    test: verify proof with email and tx body commitments

--- utils/tests/ark_verifier_test.rs
+++ utils/tests/ark_verifier_test.rs
@@ -1,33 +1,51 @@
 #[cfg(test)]
 mod tests {
     use ark_crypto_primitives::snark::SNARK;
-    use utils::ark_verifier::{GrothBn, GrothBnProof, GrothBnVkey, JsonDecoder, PublicInputs};
+    use utils::{
+        ark_verifier::{GrothBn, GrothBnProof, GrothBnVkey, JsonDecoder, PublicInputs},
+        commit::{calculate_email_commitment, calculate_tx_body_commitment},
+    };
 
     #[test]
     fn should_verify_body_proof() {
-        let vkey_json_path = "tests/data/body/vkey.json";
-        let proof_json_path = "tests/data/body/proof.json";
-        let public_inputs_json_path = "tests/data/body/public.json";
-
-        let vkey = GrothBnVkey::from_json_file(&vkey_json_path);
-        let proof = GrothBnProof::from_json_file(&proof_json_path);
-        let public_inputs: PublicInputs<3> = PublicInputs::from_json_file(&public_inputs_json_path);
-
-        let verified = GrothBn::verify(&vkey, &public_inputs, &proof).unwrap();
-        assert!(verified);
+        assert_verification(
+            "tests/data/body/vkey.json",
+            "tests/data/body/proof.json",
+            "tests/data/body/public.json",
+        );
     }
 
     #[test]
     fn should_verify_header_proof() {
-        let vkey_json_path = "tests/data/header/vkey.json";
-        let proof_json_path = "tests/data/header/proof.json";
-        let public_inputs_json_path = "tests/data/header/public.json";
+        assert_verification(
+            "tests/data/header/vkey.json",
+            "tests/data/header/proof.json",
+            "tests/data/header/public.json",
+        );
+    }
 
-        let vkey = GrothBnVkey::from_json_file(&vkey_json_path);
-        let proof = GrothBnProof::from_json_file(&proof_json_path);
-        let public_inputs: PublicInputs<3> = PublicInputs::from_json_file(&public_inputs_json_path);
+    fn assert_verification(
+        vkey_json_path: &str,
+        proof_json_path: &str,
+        public_inputs_json_path: &str,
+    ) {
+        const salt: &str = "XRhMS5Nc2dTZW5kEpAB";
+        const email: &str = "thezdev1@gmail.com";
+        const tx: &str = "CrQBCrEBChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEpABCj94aW9uMWd2cDl5djZndDBwcmdzc3\
+        ZueWNudXpnZWszZmtyeGxsZnhxaG0wNzYwMmt4Zmc4dXI2NHNuMnAycDkSP3hpb24xNGNuMG40ZjM4ODJzZ3B2NWQ5ZzA2dzNxN3hzZ\
+        m51N3B1enltZDk5ZTM3ZHAwemQ4bTZscXpwemwwbRoMCgV1eGlvbhIDMTAwEmEKTQpDCh0vYWJzdHJhY3RhY2NvdW50LnYxLk5pbFB1\
+        YktleRIiCiBDAlIzSFvCNEIMmTE+CRm0U2Gb/0mBfb/aeqxkoPweqxIECgIIARh/EhAKCgoFdXhpb24SATAQwJoMGg54aW9uLXRlc3R\
+        uZXQtMSCLjAo=";
 
+        let vkey = GrothBnVkey::from_json_file(vkey_json_path);
+        let proof = GrothBnProof::from_json_file(proof_json_path);
+        let public_inputs: PublicInputs<3> = PublicInputs::from_json_file(public_inputs_json_path);
         let verified = GrothBn::verify(&vkey, &public_inputs, &proof).unwrap();
+        let email_commitment = calculate_email_commitment(salt, email);
+        let tx_body_commitment = calculate_tx_body_commitment(tx);
+
         assert!(verified);
+        assert_eq!(public_inputs[0], tx_body_commitment);
+        assert_eq!(public_inputs[1], email_commitment);
     }
 }
