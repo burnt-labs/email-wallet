d10c825723bdffb175da4c12039580fbc64f8074
zkFriendly <zkfriendly@proton.me>
2024-05-01 09:46:18 +0200

    test: test proof verification for header email

--- utils/tests/ark_verifier_test.rs
+++ utils/tests/ark_verifier_test.rs
@@ -16,4 +16,18 @@ mod tests {
         let verified = GrothBn::verify(&vkey, &public_inputs, &proof).unwrap();
         assert!(verified);
     }
+
+    #[test]
+    fn should_verify_header_proof() {
+        let vkey_json_path = "tests/data/header/vkey.json";
+        let proof_json_path = "tests/data/header/proof.json";
+        let public_inputs_json_path = "tests/data/header/public.json";
+
+        let vkey = GrothBnVkey::from_json_file(&vkey_json_path);
+        let proof = GrothBnProof::from_json_file(&proof_json_path);
+        let public_inputs: PublicInputs<3> = PublicInputs::from_json_file(&public_inputs_json_path);
+
+        let verified = GrothBn::verify(&vkey, &public_inputs, &proof).unwrap();
+        assert!(verified);
+    }
 }
